b4d2ee6f3bef5c3af26ea983554c877c4680d699
kiselev <kiselev@NTC-36>
2020-07-13 15:48:56 +0300

    add counter Instruction and remove the test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,15 +6,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
-
-    long actual = service.findMax(incomesInBillions);
-
-    assertEquals(expected, actual);
   }
-}
